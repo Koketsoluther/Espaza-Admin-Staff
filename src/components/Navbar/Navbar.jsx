b692af259fa3cd745bb16bf180eff06e1d35c339
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-23 18:11:06 +0200

    profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-
+import { useAuth0 } from "@auth0/auth0-react";
+import UserProfile from '../UserProfile';
 const Navbar = () => {
     return (
         <div className = 'navbar'>
             <h1>E-spaza Admin </h1>
             <img className= 'profile' src = {assets.profile_icon} alt = ""/>
+            <UserProfile/>
 
         </div>
     )
