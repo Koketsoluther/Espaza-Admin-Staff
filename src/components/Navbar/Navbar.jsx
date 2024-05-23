662b142206f88ba70bc7716198852f55d3f26655
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-23 18:18:03 +0200

    remove unused

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-import { useAuth0 } from "@auth0/auth0-react";
 import UserProfile from '../UserProfile';
 const Navbar = () => {
     return (
@@ -9,7 +8,6 @@ const Navbar = () => {
             <h1>E-spaza Admin </h1>
             <img className= 'profile' src = {assets.profile_icon} alt = ""/>
             <UserProfile/>
-
         </div>
     )
 }
