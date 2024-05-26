6c5a8650abe4e140cb709dbc64646c64f9ddf212
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-26 20:05:17 +0200

    t

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-import LogoutButton from '../LogoutButton';
 const Navbar = () => {
     return (
         <div className = 'navbar'>
             <h1>E-spaza Admin </h1>
             <img className= 'profile' src = {assets.profile_icon} alt = ""/>
-            <LogoutButton/>
+        
         </div>
     )
 }
