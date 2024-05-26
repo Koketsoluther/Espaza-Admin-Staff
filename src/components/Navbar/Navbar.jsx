ef0303c33cda11913ee9b18a409c32fb8c6f2a47
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-05-26 19:59:13 +0200

    fix failed tests

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
-import UserProfile from '../UserProfile';
 import LogoutButton from '../LogoutButton';
 const Navbar = () => {
     return (
         <div className = 'navbar'>
             <h1>E-spaza Admin </h1>
             <img className= 'profile' src = {assets.profile_icon} alt = ""/>
-            <UserProfile/>
             <LogoutButton/>
         </div>
     )
